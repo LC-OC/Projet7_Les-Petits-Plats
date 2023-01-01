654fce3ec74f8f2f2fdeb2355c4ae4fc2fa3ed94
Louise Chartier <louise-chartier@outlook.fr>
2023-01-01 14:18:51 +0100

    remove comments

--- scripts/pages/searchBar.js
+++ scripts/pages/searchBar.js
@@ -2,13 +2,11 @@ import { recipes } from "../data/recipes.js";
 import {
   errorSearch,
   searchBar,
-  recipesSection,
   cards,
   lists,
   errorAppareils,
   errorUstensils,
   errorIngredients,
-  searchElement,
 } from "./DOM.js";
 import { displayRecipes } from "./index.js";
 
@@ -38,27 +36,6 @@ searchBar.addEventListener("keyup", (e) => {
     }
   });
   displayRecipes(filteredRecipes);
-  /*test first algo
-  for (let a = 0; a < searchElement.length; a++) {
-    if (
-      !searchElement[a].innerHTML.toLowerCase().includes(searchBarValue) &&
-      searchBarValue.length >= 3
-    ) {
-      searchElement[a].style.display = "none";
-      searchElement[a].classList.add("hide-recipe");
-      searchElement[a].classList.remove("show-recipe");
-    } else {
-      searchElement[a].style.display = "";
-      matchFound = true;
-      searchElement[a].classList.add("show-recipe");
-      searchElement[a].classList.remove("hide-recipe");
-    }
-  }
-  if (!matchFound) {
-    errorSearch.style.display = "block";
-  } else {
-    errorSearch.style.display = "none";
-  }*/
 });
 
 // maj listing on search
